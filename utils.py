782cf846cda699dfef5f3cb748f7b8ef7c484b1b
Stéphane <sudoweezy@gmail.com>
2025-01-02 17:09:54 +0000

    fix print

--- utils.py
+++ utils.py
@@ -15,5 +15,10 @@ def client_configuration():
 def account_creation(client: AlgodClient, name: str, funds=0):
     account = get_account(client, name) if funds == 0 else get_account(client, name, fund_with_algos=funds)
     info = client.account_info(account.address)
-    print(f'Name\t\t: %s \nAddress\t\t: %s\nCreated Asset\t: %s\nAssets\t\t: %s\nAlgo\t\t: %.6f \n' % (name, account.address,info["created-assets"], info["assets"], info["amount"] / 1_000_000))
+    print(f'Name\t\t: %s \n'
+      f'Address\t\t: %s\n'
+      f'Created Asset\t: %s\n'
+      f'Assets\t\t: %s\n'
+      f'Algo\t\t: %.6f \n' % 
+      (name, account.address, info["created-assets"], info["assets"], info["amount"] / 1_000_000))    
     return account
