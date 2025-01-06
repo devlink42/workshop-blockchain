da769543ada01a47e0ffc86b3023024bebf7a8a8
Stéphane <sudoweezy@gmail.com>
2025-01-06 14:30:27 +0100

    add func to util

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
+from typing import List
 from algokit_utils import (
     get_algod_client,
     get_default_localnet_config,
     get_account,
+    get_indexer_client
 )
 from algosdk.v2client.algod import AlgodClient
 
@@ -12,16 +14,26 @@ def client_configuration():
     return client
 
 
+def indexer_configuration():
+    config = get_default_localnet_config("indexer")
+    client = get_indexer_client(config)
+    return client
+
+
 def account_creation(client: AlgodClient, name: str, funds=0):
-    account = get_account(client, name) if funds == 0 else get_account(client, name, fund_with_algos=funds)
+    account = get_account(client, name) if funds == 0 else get_account(
+        client, name, fund_with_algos=funds)
     info = client.account_info(account.address)
+
     print(f'Name\t\t: %s \n'
-      f'Address\t\t: %s\n'
-      f'Created Asset\t: %s\n'
-      f'Assets\t\t: %s\n'
-      f'Algo\t\t: %.6f \n' % 
-      (name, account.address, info["created-assets"], info["assets"], info["amount"] / 1_000_000))    
-    return account
+          f'Address\t\t: %s\n'
+          f'Created Asset\t: %s\n'
+          f'Assets\t\t: %s\n'
+          f'Algo\t\t: %.6f' %
+          (name, account.address, info["created-assets"], info["assets"], info["amount"] / 1_000_000))
+    if len(info["created-apps"]) > 0:
+        print(f'Created-Apps\t: %s \n' % info["created-apps"][0]["id"])
+    print("")
 
 
 def get_asa_id(ptx):
@@ -29,3 +41,8 @@ def get_asa_id(ptx):
         return ptx["asset-index"]
     else:
         raise ValueError("Unexpected response from pending_transaction_info")
+
+
+def display_info(client: AlgodClient, names: List[str]):
+    for name in names:
+        account_creation(client, name)
