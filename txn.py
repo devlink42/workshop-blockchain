74bf74501fad68dc0a5d671e36b284a1894bc56f
Stéphane <sudoweezy@gmail.com>
2025-02-19 18:07:24 +0100

    push tp 1 2 3

--- txn.py
+++ txn.py
@@ -14,17 +14,36 @@ if __name__ == "__main__":
     alice = account_creation(algorand, "ALICE", au.AlgoAmount(algo=10_000))
     bob = account_creation(algorand, "BOB", au.AlgoAmount(algo=100))
 
-    print(sdk.mnemonic.from_private_key(alice.private_key))
+    # print(sdk.mnemonic.from_private_key(alice.private_key))
 
-    pay_txn = algorand.send.payment(
+    pay_txn = algorand.create_transaction.payment(
         au.PaymentParams(
             sender=alice.address,
             receiver=bob.address,
-            signer=alice.signer,
             amount=au.AlgoAmount(algo=1)
         )
     )
-    print('Transaction confirmed, round: '
-          f'{pay_txn.confirmation["confirmed-round"]}')
+    pay_txn_signed = pay_txn.sign(alice.private_key)
 
-    display_info(algorand, ["ALICE", "BOB"])
+    tx_id = algorand.client.algod.send_transaction(pay_txn_signed)
+
+    res = sdk.transaction.wait_for_confirmation(
+        algod_client=algorand.client.algod,
+        txid=tx_id
+        )
+    print('Transaction confirmed'
+          f'{res}')
+
+    # display_info(algorand, ["ALICE", "BOB"])
+
+    payback_txn = algorand.send.payment(
+        au.PaymentParams(
+            sender=bob.address,
+            signer=bob.signer,
+            receiver=alice.address,
+            amount=au.AlgoAmount(algo=1)
+        )
+    )
+
+    print('Transaction confirmed, round: '
+          f'{payback_txn.confirmation["confirmed-round"]}')
